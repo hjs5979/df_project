a6618b1955700de398612c92125274a7445ae53d
hjs5979 <hjs5979@gmail.com>
2023-05-02 18:05:28 +0900

    0502

--- difi/urls.py
+++ difi/urls.py
@@ -6,6 +6,7 @@ app_name = 'difi'
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('search/',views.search, name='search'),
+    path('', views.search, name='search'),
+    # path('search/',views.search, name='search'),
     # path('add/<int:stock_info_id>', views.stock_add, name='stock_add'),
 ]
--- difi/views.py
+++ difi/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import stock_info, stock_value
 from .forms import UpperForm
+from django.db.models import Q
 # import FinanceDataReader as fdr
 # import pandas as pd
 # import numpy as np
@@ -11,7 +12,16 @@ def index(request):
     return render(request, 'difi/upper.html')
 
 def search(request):
-    return render(request, 'difi/search.html')
+    # kw = request.GET.get('kw', '')
+    kw = '삼성전자'
+    stock_info_list = stock_info.objects
+    if kw:
+        stock_info_list = stock_info_list.filter(
+            Q(ticker__icontains=kw) |  # 제목 검색
+            Q(stock_name__icontains=kw)
+        ).distinct()
+    context = {'stock_info_list':stock_info_list,'kw': kw}
+    return render(request, 'difi/upper.html', context)
     
 
 # def stock_add(request, stock_info_id):
